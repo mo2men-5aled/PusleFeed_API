c8f21a48e69156017a1fb058999c219319524b8f
Momen <momenk208@gmail.com>
2024-06-25 08:39:06 +0300

    solve the cors origin problem

--- authentication/src/index.ts
+++ authentication/src/index.ts
@@ -1,6 +1,17 @@
 import app from "./app";
 import mongoose from "mongoose";
 
+// solve the cors origin problem
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
+});
+
 const PORT = process.env.PORT;
 const MONGO_URI = process.env.MONGO_URI as string;
 
